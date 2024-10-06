e872935f0e90cc73176f5d029b5c103290c53085
Account 1 <shakljoka103@gmail.com>
2024-10-06 20:29:02 +0600

    dashboard layout problem solved

--- resources/views/backend/admin-dashboard/layouts/master.blade.php
+++ resources/views/backend/admin-dashboard/layouts/master.blade.php
@@ -24,64 +24,46 @@
 </head>
 
 <body id="page-top">
-    <!-- Page Wrapper -->
-    <div id="wrapper">
+   <!-- Page Wrapper -->
+   <div id="wrapper">
 
-        <!-- Sidebar -->
-        @include('backend.admin-dashboard.partials.sidebar')
+    <!-- Sidebar -->
+    @include('backend.admin-dashboard.partials.sidebar')
+    <!-- End of Sidebar -->
 
-        <!-- Content Wrapper -->
-        <div class="content">
-            <div>
-                <div id="content-wrapper" class="d-flex flex-column">
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
 
-                    <!-- Main Content -->
-                    <div id="content">
+        <!-- Main Content -->
+        <div id="content">
 
-                        <!-- Topbar -->
-                        @include('backend.admin-dashboard.partials.navbar')
+            <!-- Topbar -->
+            @include('backend.admin-dashboard.partials.navbar')
+            <!-- End of Topbar -->
 
-                        {{-- DASHBOARD MAIN DATA --}}
-                        @yield('content')
+            <!-- Begin Page Content -->
+            @yield('content')
+            <!-- /.container-fluid -->
 
+        </div>
+        <!-- End of Main Content -->
 
-                    </div>
-
-                    <!-- Footer -->
-                    @include('backend.admin-dashboard.partials.footer')
-                    <!-- End of Footer -->
-
-                </div>
-
-                <!-- Scroll to Top Button-->
-                <a class="scroll-to-top rounded" href="#page-top">
-                    <i class="fas fa-angle-up"></i>
-                </a>
-
-                <!-- Logout Modal-->
-                <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog"
-                    aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">×</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">Select "Logout" below if you are ready to end your current session.
-                            </div>
-                            <div class="modal-footer">
-                                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                                <a class="btn btn-primary" href="login.html">Logout</a>
-                            </div>
-                        </div>
-                    </div>
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Copyright &copy; Your Website 2020</span>
                 </div>
             </div>
-        </div>
+        </footer>
+        <!-- End of Footer -->
 
     </div>
+    <!-- End of Content Wrapper -->
+
+
+</div>
+<!-- End of Page Wrapper -->
 
     <!-- Bootstrap core JavaScript-->
     <script src="{{ asset('backend/vendor/jquery/jquery.min.js') }}"></script>
--- resources/views/backend/admin-dashboard/partials/navbar.blade.php
+++ resources/views/backend/admin-dashboard/partials/navbar.blade.php
@@ -1,12 +1,15 @@
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
     <!-- Sidebar Toggle (Topbar) -->
-    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-        <i class="fa fa-bars"></i>
-    </button>
+    <form class="form-inline">
+        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+        </button>
+    </form>
 
     <!-- Topbar Search -->
-    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+    <form
+        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
         <div class="input-group">
             <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
                 aria-label="Search" aria-describedby="basic-addon2">
@@ -33,7 +36,8 @@
                 <form class="form-inline mr-auto w-100 navbar-search">
                     <div class="input-group">
                         <input type="text" class="form-control bg-light border-0 small"
-                            placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                            placeholder="Search for..." aria-label="Search"
+                            aria-describedby="basic-addon2">
                         <div class="input-group-append">
                             <button class="btn btn-primary" type="button">
                                 <i class="fas fa-search fa-sm"></i>
@@ -66,8 +70,7 @@
                     </div>
                     <div>
                         <div class="small text-gray-500">December 12, 2019</div>
-                        <span class="font-weight-bold">A new monthly report is ready to
-                            download!</span>
+                        <span class="font-weight-bold">A new monthly report is ready to download!</span>
                     </div>
                 </a>
                 <a class="dropdown-item d-flex align-items-center" href="#">
@@ -92,8 +95,7 @@
                         Spending Alert: We've noticed unusually high spending for your account.
                     </div>
                 </a>
-                <a class="dropdown-item text-center small text-gray-500" href="#">Show All
-                    Alerts</a>
+                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
             </div>
         </li>
 
@@ -113,7 +115,7 @@
                 </h6>
                 <a class="dropdown-item d-flex align-items-center" href="#">
                     <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="{{ asset('backend/img/undraw_profile_1.svg') }}"
+                        <img class="rounded-circle" src="img/undraw_profile_1.svg"
                             alt="...">
                         <div class="status-indicator bg-success"></div>
                     </div>
@@ -125,7 +127,7 @@
                 </a>
                 <a class="dropdown-item d-flex align-items-center" href="#">
                     <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="{{ asset('backend/img/undraw_profile_2.svg') }}"
+                        <img class="rounded-circle" src="img/undraw_profile_2.svg"
                             alt="...">
                         <div class="status-indicator"></div>
                     </div>
@@ -137,13 +139,12 @@
                 </a>
                 <a class="dropdown-item d-flex align-items-center" href="#">
                     <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="{{ asset('backend/img/undraw_profile_3.svg') }}"
+                        <img class="rounded-circle" src="img/undraw_profile_3.svg"
                             alt="...">
                         <div class="status-indicator bg-warning"></div>
                     </div>
                     <div>
-                        <div class="text-truncate">Last month's report looks great, I am very happy
-                            with
+                        <div class="text-truncate">Last month's report looks great, I am very happy with
                             the progress so far, keep up the good work!</div>
                         <div class="small text-gray-500">Morgan Alvarez · 2d</div>
                     </div>
@@ -160,8 +161,7 @@
                         <div class="small text-gray-500">Chicken the Dog · 2w</div>
                     </div>
                 </a>
-                <a class="dropdown-item text-center small text-gray-500" href="#">Read More
-                    Messages</a>
+                <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
             </div>
         </li>
 
@@ -172,7 +172,8 @@
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                <img class="img-profile rounded-circle" src="{{ asset('backend/img/undraw_profile.svg') }}">
+                <img class="img-profile rounded-circle"
+                    src="{{asset('backend/img/undraw_profile.svg')}}">
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
